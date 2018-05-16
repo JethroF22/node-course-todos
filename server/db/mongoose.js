d8cb5294331e69f9d3e7911faa89b769d6aa5f6b
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-05-16 15:15:10 +0200

    Test the DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri = process.env.MONGODB_URL || "mongodb://localhost:27017/TodoApp";
+console.log(uri);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
